5216b333d14a1d5e2dce83404de654fc6db8daab
Junji Hashimoto <junji.hashimoto@gmail.com>
2014-10-02 17:27:36 +0900

    Added filterClause for PersistQuery

--- persistent-zookeeper/Database/Persist/Zookeeper.hs
+++ persistent-zookeeper/Database/Persist/Zookeeper.hs
@@ -2,10 +2,12 @@ module Database.Persist.Zookeeper
     ( module Database.Persist.Zookeeper.Config
     , module Database.Persist.Zookeeper.Store
     , module Database.Persist.Zookeeper.Unique
+    , module Database.Persist.Zookeeper.Query
     , module Database.Persist.Zookeeper.PathPiece
     ) where
 
 import Database.Persist.Zookeeper.Config
 import Database.Persist.Zookeeper.Store
 import Database.Persist.Zookeeper.Unique()
+import Database.Persist.Zookeeper.Query
 import Database.Persist.Zookeeper.PathPiece()
